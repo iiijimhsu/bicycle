6cf1211af1f77be70cd89c9ff6b8e4aa2bbf4293
Carl <carlhuang@sunnygo.com.tw>
2022-04-17 11:00:28 +0800

    third commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
-import React from 'react'
-
+import React, { useState, useEffect } from 'react'
+import { getData } from '../src/api'
+import jsSHA from "jssha";
+import axios from "axios";
 
 export default function App() {
+
+
+  axios.get(
+    "https://ptx.transportdata.tw/MOTC/v2/Tourism/Bus/EstimatedTimeOfArrival/TaiwanTrip?%24top=30&%24format=JSON",
+    {
+      headers: getAuthorizationHeader()
+    }
+  )
+    .then(function (response) {
+      console.log(response)
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  function getAuthorizationHeader() {
+    //  填入自己 ID、KEY 開始
+    let AppID = 'a465ec9e862b4c6cb1388215b593a2d0';
+    let AppKey = 'D1VEDZ9dYJ6pkyxDK2QzK47GY3I';
+    //  填入自己 ID、KEY 結束
+    let GMTString = new Date().toGMTString();
+    let ShaObj = new jsSHA('SHA-1', 'TEXT');
+    ShaObj.setHMACKey(AppKey, 'TEXT');
+    ShaObj.update('x-date: ' + GMTString);
+    let HMAC = ShaObj.getHMAC('B64');
+    let Authorization = 'hmac username="' + AppID + '", algorithm="hmac-sha1", headers="x-date", signature="' + HMAC + '"';
+    return { 'Authorization': Authorization, 'X-Date': GMTString };
+  }
+
 
   return (
     <>
