fdcad720661505ea6fb1dd7e6867b9a4bd1794cf
Carl <carlhuang@sunnygo.com.tw>
2022-04-17 12:02:04 +0800

    add Lottie sample

--- src/App.js
+++ src/App.js
@@ -1,50 +1,37 @@
 import React, { useState, useEffect } from 'react'
 import { getData } from '../src/api'
-import jsSHA from "jssha";
-import axios from "axios";
+import Lottie from "react-lottie";
+import centerImage from "../src/youbike.json";
+
 
 export default function App() {
-  const [test,setTest]=useState([])
-  const [testButton,setTestButton]=useState(false)
-  useEffect(()=>{
-    getData("https://ptx.transportdata.tw/MOTC/v2/Tourism/Bus/EstimatedTimeOfArrival/TaiwanTrip?%24top=30&%24format=JSON",setTest)
-    console.log("test",test)
-  },[testButton])
-
-  // axios.get(
-  //   "https://ptx.transportdata.tw/MOTC/v2/Tourism/Bus/EstimatedTimeOfArrival/TaiwanTrip?%24top=30&%24format=JSON",
-  //   {
-  //     headers: getAuthorizationHeader()
-  //   }
-  // )
-  //   .then(function (response) {
-  //     console.log(response)
-  //   })
-  //   .catch(function (error) {
-  //     console.log(error);
-  //   });
-  // function getAuthorizationHeader() {
-  //   //  填入自己 ID、KEY 開始
-  //   let AppID = 'a465ec9e862b4c6cb1388215b593a2d0';
-  //   let AppKey = 'D1VEDZ9dYJ6pkyxDK2QzK47GY3I';
-  //   //  填入自己 ID、KEY 結束
-  //   let GMTString = new Date().toGMTString();
-  //   let ShaObj = new jsSHA('SHA-1', 'TEXT');
-  //   ShaObj.setHMACKey(AppKey, 'TEXT');
-  //   ShaObj.update('x-date: ' + GMTString);
-  //   let HMAC = ShaObj.getHMAC('B64');
-  //   let Authorization = 'hmac username="' + AppID + '", algorithm="hmac-sha1", headers="x-date", signature="' + HMAC + '"';
-  //   return { 'Authorization': Authorization, 'X-Date': GMTString };
-  // }
+  const [test, setTest] = useState([])
+  const [testButton, setTestButton] = useState(false)
+  useEffect(() => {
+    getData("https://ptx.transportdata.tw/MOTC/v2/Tourism/Bus/EstimatedTimeOfArrival/TaiwanTrip?%24top=30&%24format=JSON", setTest)
+    console.log("test", test)
+  }, [testButton])
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: centerImage,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
 
 
   return (
     <>
       <div>hello,React</div>
-      <button onClick={()=>{
+      <button onClick={() => {
         setTestButton(!testButton)
       }}>test</button>
-
+      <Lottie
+        options={defaultOptions}
+        height={110}
+        width={200}
+      />
     </>
   )
 }
